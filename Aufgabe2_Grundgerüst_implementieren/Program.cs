dc3b7e81fb4698a22ac523c36e026db0b2475ec9
Kander Akinci <87019962+AI-Assistant@users.noreply.github.com>
2026-06-29 08:47:48 +0200

    Remove hotel check-in logic from Program.cs
    Removed the implementation of the hotel check-in logic.

--- Aufgabe2_Grundgerüst_implementieren/Program.cs
+++ Aufgabe2_Grundgerüst_implementieren/Program.cs
@@ -16,25 +16,5 @@ public static class Aufgabe_2
     {
         //Diese Zeile mit Ihrem Quellcode ersetzen
 
-        double zimmerpreis = 70;
-        double mwst = 0.19;
-
-        int anzahlNaechte = 0;
-        int anz_personen = 0;
-        double gesamtpreis = 0;
-        double netto_preis = 0;
-
-        Console.WriteLine("Willkommen im Hotel Check-In App");
-        Console.WriteLine("Bitte geben Sie die Anzahl der Nächte ein:");
-        anzahlNaechte = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine("Bitte geben Sie die Anzahl der Personen ein:");
-        anz_personen = Convert.ToInt32(Console.ReadLine());
-
-        netto_preis = zimmerpreis * anzahlNaechte * anz_personen;
-        gesamtpreis = netto_preis * (1 + mwst);
-
-        Console.WriteLine($"Der Gesamtpreis beträgt:{gesamtpreis,10:F2} EUR");
-
     }
 }
